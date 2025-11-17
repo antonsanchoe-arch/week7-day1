4c2e86a45cd514d16ba66479916dd9b01ac0a779
antonsanchoe-arch <anton.sancho.e@gmail.com>
2025-11-17 14:51:51 +0100

    done

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 
+//import project json
+const projects = require("./data/projects.json");
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
+const app = express();
 
 
 
@@ -13,13 +19,42 @@
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
-
+app.use(express.static("public"));
+app.use(express.json());
+app.use(morgan("dev"));
 
 
 // ROUTES
 // Start defining your routes here:
+//home
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+// Blog
+app.get("/blog", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "blog.html"));
+});
+
+// API Projects
+app.get("/api/projects", (req, res) => {
+  res.json(projects);
+});
+
+// API Articles
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
+});
+
+// error
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "views", "not-found.html"));
+});
 
 
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => {
+  console.log("Servidor en http://localhost:5005");
+});
